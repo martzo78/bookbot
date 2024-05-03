4abf3c59c8b8773df5ad7dcbdebc59751ac99b87
martzo78 <mb@mbrx.de>
2024-05-03 20:20:27 +0200

    added sort method to get sorted output

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ def main():
         text = textfile.read()
     number_of_words = countwords(text)
     dict_of_letters = countletters(text)
+    sorted_dict_of_letters = sort(dict_of_letters)
     
     print(f"--- Begin report of {path} ---")
     print(f"{number_of_words} words found in the document")
     print("")
 
-    print(dict_of_letters)
+    for letter in sorted_dict_of_letters:
+        print(f"The '{letter}' was found {dict_of_letters[letter]} times")
+
+    print("--- End report ---")
 
     return
 
@@ -37,4 +41,21 @@ def countletters(text):
     
     return dict
 
+def sort_on(dict):
+    return dict['num']
+
+def sort(dict):
+
+    list = []
+    for entry in dict:
+        list.append({'letter': entry, 'num': dict[entry]})
+    
+    list.sort(reverse=True, key=sort_on)
+    
+    new_dict = {}
+    for entry in list:
+        new_dict[entry['letter']] = entry['num']
+
+    return new_dict
+    
 main()
