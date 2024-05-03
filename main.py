d80e3b05ef9557928f6366fd2c00054c6f288dea
martzo78 <mb@mbrx.de>
2024-05-03 19:51:39 +0200

    Added function to count letters in text.

--- main.py
+++ main.py
@@ -1,12 +1,40 @@
 def main():
+
     path = "books/frankenstein.txt"
     with open(path) as textfile:
         text = textfile.read()
     number_of_words = countwords(text)
-    print (f"Der Text besteht aus {number_of_words} Wörtern.")
+    dict_of_letters = countletters(text)
+    
+    print(f"--- Begin report of {path} ---")
+    print(f"{number_of_words} words found in the document")
+    print("")
+
+    print(dict_of_letters)
+
+    return
+
 
 def countwords(text):
+
     words = text.split()
+
     return len(words)
+
+def countletters(text):
+
+    dict = {}
+    
+    text = text.lower()
+    words = text.split()
+
+    for word in words:
+        for letter in word:
+            if letter in dict.keys():
+                dict[letter] = dict[letter] + 1
+            elif letter.isalpha():
+                dict[letter] = 1
+    
+    return dict
 
 main()
